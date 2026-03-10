dea4f7b2591a16776dc8551d337da4570b06008c
Duypheloi <nstcrystal@gmail.com>
2026-03-10 16:00:16 +0700

    Fix: Discord webhook API call function

--- src/app/components/ContactForm.tsx
+++ src/app/components/ContactForm.tsx
@@ -105,7 +105,7 @@ export function ContactForm() {
 
   const sendMessage = async (data: ContactFormData) => {
 
-    const response = await fetch("https://webhook-discord-qtkh.onrender.com", {
+    const response = await fetch("https://webhook-discord-qtkh.onrender.com/api/contact", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
