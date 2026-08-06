433554faeb53010410f2243d3b43b35f11c5ed63
nstcrystal <nstcrystal@gmail.com>
2026-08-06 13:40:20 +0700

    Restart

--- env.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="vite/client" />
-
-declare module '*.md' {
-  import type { ComponentOptions } from 'vue'
-  const component: ComponentOptions
-  export default component
-  export const frontmatter: Record<string, any>
-}
--- src/posts/Hello.md
+++
@@ -1,9 +0,0 @@
----
-title: Xin chào thế giới
-date: 2026-07-01
-description: Đây là bài viết đầu tiên của tôi.
----
-
-# Nội dung bài viết ở đây...
-
-Hello
