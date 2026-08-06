647bd3e0acf2bd3dd2b349b63dba37ef2c6d3be6
nstcrystal <nstcrystal@gmail.com>
2026-08-06 13:58:06 +0700

    feat: update favicon to NSTCrystal.png and add new asset

--- app/app.vue
+++ app/app.vue
@@ -10,7 +10,7 @@ useHead({
     { key: 'theme-color', name: 'theme-color', content: color }
   ],
   link: [
-    { rel: 'icon', href: '/favicon.ico' }
+    { rel: 'icon', href: '/NSTCrystal.png' }
   ],
   htmlAttrs: {
     lang: 'vi'
