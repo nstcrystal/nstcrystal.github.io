e19dbbe37fdd9ef48a83524a946a957eddce16ae
nstcrystal <nstcrystal@gmail.com>
2026-08-26 22:09:43 +0700

    feat: add google site verification meta tag for SEO

--- app/app.vue
+++ app/app.vue
@@ -7,6 +7,7 @@ useHead({
   meta: [
     { charset: 'utf-8' },
     { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    { name: 'google-site-verification', content: 'ZBWLDCgU_uMKcEb4tb1TnszttyTNdEFW2LKEC2OQxS0' },
     { key: 'theme-color', name: 'theme-color', content: color }
   ],
   link: [
